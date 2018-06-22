4832d406f911c8b7f2b345735d41b414f4c1219c
Christopher Parsons <mail@christopherparsons.me>
2018-06-22 21:55:01 +0100

    Wordwrap kata complete Kata completed whilst following along with video: https://www.youtube.com/watch?v=LznGFLxqmnA

--- lib/wordwrap.rb
+++ lib/wordwrap.rb
@@ -1,10 +1,20 @@
 class Wordwrap
   def self.wrap(string, wrapping_point)
     # return string if string.length <= wrapping_point
+    # ^^^
+    # For some reason the above line breaks my tests completely if I use this
+    # instead of the if/else
     if string.length <= wrapping_point
       string
     else
-      string[0...wrapping_point] + "\n" + string[wrapping_point..-1]
+      break_column = string[0...wrapping_point].rindex(" ") || wrapping_point
+      #https://teamtreehouse.com/community/index-vs-rindex
+      string[0...break_column].strip + "\n" + wrap(string[break_column..-1].strip, wrapping_point)
+      #https://apidock.com/rails/Array/wrap/class
     end
   end
 end
+
+#Focus is to find location of newline, then work to find whether there is a
+# relevant space to put the newline. (Rather than focussing on spaces and then
+# dealing with mid-word splits as an exception).
--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -27,12 +27,60 @@ describe Wordwrap do
         expect(Wordwrap.wrap("word", 2)).to eq("wo\nrd")
       end
     end
+
+    describe "With a line length limit that requires more than one new line" do
+      it "Returns the string with new lines inserted" do
+        expect(Wordwrap.wrap("longword", 3)).to eq("lon\ngwo\nrd")
+      end
+    end
   end
 
   context "With a string with content and spaces" do
     describe "With a line length limit greater than the string length" do
       it "Returns the string unchanged" do
         expect(Wordwrap.wrap("Another test", 13)).to eq("Another test")
+      end
+    end
+
+    describe "With a line length limit after the space" do
+      it "Returns the string with a line break at the space" do
+        expect(Wordwrap.wrap("long word", 5)).to eq("long\nword")
+      end
+    end
+
+    describe "With a line length limit on the space" do
+      it "Returns the string with a line break at the space" do
+        expect(Wordwrap.wrap("long word", 4)).to eq("long\nword")
+      end
+    end
+
+    describe "With a line length limit in the second word" do
+      it "Returns the string with a line break at the space" do
+        expect(Wordwrap.wrap("long word", 6)).to eq("long\nword")
+      end
+    end
+
+    describe "With multiple spaces and line breaks" do
+      it "Returns the string on multiple lines" do
+        expect(Wordwrap.wrap("long long word", 6)).to eq("long\nlong\nword")
+      end
+    end
+
+    describe "With multiple spaces but only one line break" do
+      it "Returns the string on two lines" do
+        expect(Wordwrap.wrap("long long word", 10)).to eq("long long\nword")
+      end
+    end
+
+    describe "With multiple spaces but only one line break" do
+      it "Returns the string on two lines" do
+        expect(Wordwrap.wrap("long long long word", 16)).to eq("long long long\nword")
+      end
+    end
+
+    describe "With multiple spaces but only one line break" do
+      it "Returns the string on two lines" do
+        expect(Wordwrap.wrap("long long long word", 10)).to eq("long long\nlong word")
       end
     end
   end
